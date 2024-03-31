8cd073143b89342a7be42c9ac0635d95d485d4d0
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-03-31 22:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const extendHex = (shortHex) => {
   // write your code here
+	 shortHex = shortHex.replace('#', '');
+
+    // Check if the hex code is short
+    if (shortHex.length === 3) {
+        // Extend the hex code
+        shortHex = shortHex[0] + shortHex[0] + shortHex[1] + shortHex[1] + shortHex[2] + shortHex[2];
+    }
+
+    // Return the full hex code with the '#' prefix
+    return '#' + shortHex;
 };
 
 // Do not change the code below.
